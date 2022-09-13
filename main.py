22c171032131dc19911f46f6f1e1117b45020df2
fedulovs <heaven45@mail.ru>
2022-09-13 22:25:48 +0300

    load data from backend

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
-from PyQt5.uic import loadUi
+
+import requests
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.uic import loadUi
 
 
 class MainWindow(QDialog):
@@ -14,45 +16,42 @@ class MainWindow(QDialog):
         # set the title
         self.setWindowTitle("Device inventory")
 
-        # setting  the geometry of window
-        self.setGeometry(0, 0, 900, 1300)
+        # setting the geometry of window
+        self.setGeometry(0, 0, 900, 2000)
 
-        self.tableWidget.resize(1000, 700)
+        # button settings
+        self.addDevice.setStyleSheet(
+            "border-radius : 50; border: 2 px solid black; background-color : blue; color: white")
+        self.addDevice.setGeometry(200, 150, 100, 100)
+        self.addDevice.move(1000, 700)
+        self.addDevice.clicked.connect(lambda: self.add_device())
+
+        self.tableWidget.resize(950, 700)
         self.tableWidget.setColumnWidth(0, 100)
         self.tableWidget.setColumnWidth(1, 150)
         self.tableWidget.setColumnWidth(2, 120)
         self.tableWidget.setColumnWidth(3, 100)
-        self.tableWidget.setColumnWidth(4, 120)
+        self.tableWidget.setColumnWidth(4, 200)
         self.tableWidget.setColumnWidth(5, 190)
         self.load_data()
 
     def load_data(self):
-        devices = [{"brand": "Samsung", "device": "Galaxy S10", "os version": "Android 11", "owner": "Jack",
-                    "comment": "", "not compatible with": "Guitar tuna"},
-                   {"brand": "Xiaomi", "device": "Redmi Note 8T", "os version": "Android 9", "owner": "Cabinet",
-                    "comment": "", "not compatible with": ""},
-                   {"brand": "Huawei", "device": "P40 Lite", "os version": "Android 11", "owner": "Sergey",
-                    "comment": "", "not compatible with": "Guitar tuna"},
-                   {"brand": "Xiaomi", "device": "Redmi Note 7", "os version": "Android 10", "owner": "Cabinet",
-                    "comment": "Lost since 12.8.22", "not compatible with": ""},
-                   {"brand": "Xiaomi", "device": "Redmi Note 8T", "os version": "Android 9", "owner": "Cabinet",
-                    "comment": "", "not compatible with": ""},
-                   {"brand": "Xiaomi", "device": "Redmi Note 7", "os version": "Android 10", "owner": "Cabinet",
-                    "comment": "Lost since 12.8.22", "not compatible with": ""},
-                   {"brand": "Xiaomi", "device": "Redmi Note 8T", "os version": "Android 9", "owner": "Cabinet",
-                    "comment": "", "not compatible with": ""}]
+        url = "http://127.0.0.1:8000/get-items"
+        res = requests.get(url)
+        devices = res.json()
         row = 0
         self.tableWidget.setRowCount(len(devices))
         for device in devices:
             self.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(device["brand"]))
             self.tableWidget.setItem(row, 1, QtWidgets.QTableWidgetItem(device["device"]))
-            self.tableWidget.setItem(row, 2, QtWidgets.QTableWidgetItem(device["os version"]))
+            self.tableWidget.setItem(row, 2, QtWidgets.QTableWidgetItem(device["os_version"]))
             self.tableWidget.setItem(row, 3, QtWidgets.QTableWidgetItem(device["owner"]))
-            self.tableWidget.setItem(row, 4, QtWidgets.QTableWidgetItem(device["comment"]))
-            self.tableWidget.setItem(row, 5, QtWidgets.QTableWidgetItem(device["not compatible with"]))
-            row = row+1
+            self.tableWidget.setItem(row, 4, QtWidgets.QTableWidgetItem(device["comments"]))
+            self.tableWidget.setItem(row, 5, QtWidgets.QTableWidgetItem(device["not_compatible_with"]))
+            row = row + 1
 
-
+    def add_device(self):
+        print("clicked")
 
 
 app = QApplication(sys.argv)
