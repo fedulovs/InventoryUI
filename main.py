c2ae65c2b1ef8df4c0e35c9cd9157d048fd4919f
fedulovs <heaven45@mail.ru>
2022-09-18 21:08:03 +0300

    Add device dialogues

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication
 from PyQt5.uic import loadUi
 
+device = {}
+
 
 class MainWindow(QDialog):
     def __init__(self):
@@ -18,9 +20,7 @@ class MainWindow(QDialog):
         # setting the geometry of main window
         self.setGeometry(0, 0, 900, 2000)
 
-        # button settings
-        self.addDevice.setStyleSheet(
-            "border-radius : 50; border: 2 px solid black; background-color : blue; color: white")
+        # Add button settings
         self.addDevice.setGeometry(200, 150, 100, 100)
         self.addDevice.move(1000, 700)
         self.addDevice.clicked.connect(lambda: self.add_device())
@@ -50,7 +50,7 @@ class MainWindow(QDialog):
             row = row + 1
 
     def add_device(self):
-        print("clicked")
+        print("Open Device button clicked")
         add_device1 = DeviceDialog1()
         widget.addWidget(add_device1)
         widget.setFixedHeight(500)
@@ -62,18 +62,132 @@ class DeviceDialog1(QDialog):
     def __init__(self):
         super(DeviceDialog1, self).__init__()
         loadUi("add_device1.ui", self)
-        self.setGeometry(0, 0, 400, 300)
 
+        # Back button
         self.backButton.setStyleSheet(
             "border-radius : 50; border: 2 px solid black; color: black")
         self.backButton.setGeometry(10, 10, 100, 100)
         self.backButton.clicked.connect(lambda: self.go_back())
 
+        # Next button
+        self.nextButton.clicked.connect(lambda: self.open_os_input())
+
+    def open_os_input(self):
+        print("Open OS button clicked")
+        add_device2 = DeviceDialog2()
+        widget.addWidget(add_device2)
+        widget.setFixedHeight(500)
+        widget.setFixedWidth(400)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+        device['device'] = self.deviceInput.text()
+        device['brand'] = self.brandInput.text()
+        print(device)
+
     def go_back(self):
+        print("Going back to main window")
         mainwindow = MainWindow()
         widget.addWidget(mainwindow)
         widget.setFixedHeight(850)
         widget.setFixedWidth(1120)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+class DeviceDialog2(QDialog):
+    def __init__(self):
+        super(DeviceDialog2, self).__init__()
+        loadUi("add_device2.ui", self)
+
+        # Next button
+        self.nextButton.clicked.connect(lambda: self.open_comments_input())
+
+        # Back button
+        self.backButton.setGeometry(10, 10, 100, 100)
+        self.backButton.clicked.connect(lambda: self.go_back())
+
+    def open_comments_input(self):
+        print("Open Comments button clicked")
+        add_device3 = DeviceDialog3()
+        widget.addWidget(add_device3)
+        widget.setFixedHeight(500)
+        widget.setFixedWidth(400)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+        device['os'] = self.osInput.text()
+
+    def go_back(self):
+        print("Going back to screen 1")
+        add_device1 = DeviceDialog1()
+        widget.addWidget(add_device1)
+        widget.setFixedHeight(500)
+        widget.setFixedWidth(400)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+class DeviceDialog3(QDialog):
+    def __init__(self):
+        super(DeviceDialog3, self).__init__()
+        loadUi("add_device3.ui", self)
+
+        # Next button
+        self.nextButton.clicked.connect(lambda: self.open_not_compatible_input())
+
+        # Back button
+        self.backButton.setGeometry(10, 10, 100, 100)
+        self.backButton.clicked.connect(lambda: self.go_back())
+
+    def open_not_compatible_input(self):
+        print("Open Not compatible button clicked")
+        add_device4 = DeviceDialog4()
+        widget.addWidget(add_device4)
+        widget.setFixedHeight(500)
+        widget.setFixedWidth(400)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+        device['comment'] = self.commentsInput.toPlainText()
+
+    def go_back(self):
+        print("Going back to screen 2")
+        add_device2 = DeviceDialog2()
+        widget.addWidget(add_device2)
+        widget.setFixedHeight(500)
+        widget.setFixedWidth(400)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+class DeviceDialog4(QDialog):
+    def __init__(self):
+        super(DeviceDialog4, self).__init__()
+        loadUi("add_device4.ui", self)
+
+        self.dropdown.addItem('-')
+        self.dropdown.addItem('Guitar Tuna')
+        self.dropdown.addItem('Yousician')
+        self.dropdown.addItem('Campfire')
+        self.dropdown.addItem('Lessons')
+
+        # Add device button
+        self.nextButton.clicked.connect(lambda: self.open_confirmation_screen())
+
+        # Back button
+        self.backButton.setGeometry(10, 10, 100, 100)
+        self.backButton.clicked.connect(lambda: self.go_back())
+
+    def open_confirmation_screen(self):
+        print("Confirmation button clicked")
+        add_device4 = DeviceDialog4()
+        widget.addWidget(add_device4)
+        widget.setFixedHeight(500)
+        widget.setFixedWidth(400)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+        device["not_compatible_with"] = self.dropdown.currentText()
+
+        for value in device.keys():
+            print(value)
+
+    def go_back(self):
+        print("Going back to screen 3")
+        add_device2 = DeviceDialog2()
+        widget.addWidget(add_device2)
+        widget.setFixedHeight(500)
+        widget.setFixedWidth(400)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
 
@@ -87,4 +201,4 @@ widget.show()
 try:
     sys.exit(app.exec_())
 except:
-    print("exiting")
+    print("Exiting")
