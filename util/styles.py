095f9f1f4b3183f7a52786ae3207e27c3fde052b
fedulovs <heaven45@mail.ru>
2023-04-25 20:47:27 +0300

    Add stylesheet

---
+++ util/styles.py
@@ -0,0 +1,46 @@
+# Buttons
+primary_button_light = """
+QPushButton {
+border-radius: 15;
+border: none;
+background-color: #414BB2;
+color: white
+}
+
+QPushButton:hover{
+border-radius: 15;
+border: none;
+background-color: rgb(51, 60, 140);
+color: white;
+}
+"""
+
+secondary_button_light = """
+QPushButton {
+border-radius: 15;
+border: 2 px solid black;
+background-color: white;
+color: #414BB2
+}
+
+QPushButton:hover{
+border-radius: 15;
+border: 4 px solid black;
+background-color: rgb(227, 227, 227);
+color: #414BB2;
+}
+"""
+
+back_button = """
+QPushButton {
+color: white;
+border: none;
+border-radius: 10;
+}
+
+QPushButton:hover {
+border: 2 px solid black;
+background-color: rgb(227, 227, 227);
+border-radius: 10;
+}                                           
+"""
